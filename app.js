4b3ad06742c309a9d2a9674e8b616f26117499f6
Rishabh Maheshwari <79650357+Rishabh-792@users.noreply.github.com>
2022-02-27 20:36:03 +0530

    ready

--- app.js
+++ app.js
@@ -90,6 +90,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-app.listen(3000, () => {
-    console.log("Serving on port 3000");
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`server is running at port no ${port}`);
 });
